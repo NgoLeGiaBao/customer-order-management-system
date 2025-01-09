374c07688d546ea2023bc60a6fc6157e9f10e937
Ngô Lê Gia Bảo <52200053@student.tdtu.edu.vn>
2025-01-09 17:55:45 +0700

    Build back-end using Spring Boot

---
+++ back-end/customer-order-management-system/src/main/java/com/de01/customerordermanagementsystem/CustomerOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.de01.customerordermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrderManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerOrderManagementSystemApplication.class, args);
+    }
+
+}
